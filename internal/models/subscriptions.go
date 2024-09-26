9fa404253a1d9de4f2d73f6bc12cf29dae414aa2
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-09-26 17:14:58 +0530

    Subscription Unique Id Gorm Mapping

--- internal/models/subscriptions.go
+++ internal/models/subscriptions.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Subscription model, no changes needed here
 type Subscription struct {
 	ID        uint32 `gorm:"primaryKey"`
 	Name      string `gorm:"size:200;not null"`
@@ -12,24 +13,28 @@ type Subscription struct {
 	UpdatedAt time.Time
 }
 
+// UserSubscriptionMapping - combination of UserId and SubscriptionId must be unique
 type UserSubscriptionMapping struct {
 	ID             uint64 `gorm:"primaryKey"`
-	UserId         uint64 `gorm:"unique;not null"`
-	SubscriptionId uint32 `gorm:"unique;not null"`
+	UserId         uint64 `gorm:"not null;uniqueIndex:idx_user_subscription"` // Part of composite unique index
+	SubscriptionId uint32 `gorm:"not null;uniqueIndex:idx_user_subscription"` // Part of composite unique index
 }
 
+// FeatureSubscriptionMapping - combination of FeatureId and SubscriptionId must be unique
 type FeatureSubscriptionMapping struct {
 	ID             uint64 `gorm:"primaryKey"`
-	FeatureId      uint32 `gorm:"unique;not null"`
-	SubscriptionId uint32 `gorm:"unique;not null"`
+	FeatureId      uint32 `gorm:"not null;uniqueIndex:idx_feature_subscription"` // Part of composite unique index
+	SubscriptionId uint32 `gorm:"not null;uniqueIndex:idx_feature_subscription"` // Part of composite unique index
 }
 
+// UserSubscriptionHistory - combination of UserId and SubscriptionId must be unique
 type UserSubscriptionHistory struct {
 	ID               uint64 `gorm:"primaryKey"`
-	UserId           uint64 `gorm:"unique;not null"`
-	SubscriptionId   uint32 `gorm:"unique;not null"`
+	UserId           uint64 `gorm:"not null;uniqueIndex:idx_user_subscription_history"` // Part of composite unique index
+	SubscriptionId   uint32 `gorm:"not null;uniqueIndex:idx_user_subscription_history"` // Part of composite unique index
 	StartDate        time.Time
 	RenewalDate      time.Time
 	ExpiryDate       time.Time
 	NumberOfRenewals uint16
 }
+
